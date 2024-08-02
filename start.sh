f6fe8f91d24eee278a2982fc92f351c79ba5e462
ayrtonaoki <ayrtonsasouza@gmail.com>
2024-08-02 18:58:39 -0300

    Improve sh commands

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 echo "Starting React frontend..."
-PORT=3001 cd frontend-react && npm start &
+PORT=3001 cd frontend-react && npm install && npm start &
+
+echo "Creating Devise JWT Secret Key..."
+cd ./backend-rails
+SECRET=$(bundle exec rake secret)
+echo "DEVISE_JWT_SECRET_KEY=$SECRET" > .env
 
 echo "Starting Rails API..."
-PORT=3000 cd backend-rails && rails s -p 3000
+PORT=3000 rails db:create db:migrate && rails s -p 3000
