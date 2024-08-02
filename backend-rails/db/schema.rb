93a26c38fdf08e9e011ddcaf619b29b2b699aff4
ayrtonaoki <ayrtonsasouza@gmail.com>
2024-08-02 17:54:29 -0300

    Create JWT deny list

--- backend-rails/db/schema.rb
+++ backend-rails/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_08_02_204705) do
+ActiveRecord::Schema.define(version: 2024_08_02_205312) do
+
+  create_table "jwt_denylist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.index ["jti"], name: "index_jwt_denylist_on_jti"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
